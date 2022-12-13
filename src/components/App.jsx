551243d775a6f5d5e6aee0120f503ca91b706a91
YurGoV <yurgov@gmail.com>
2022-12-13 11:11:59 +0200

    1-2_steps_done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Container } from './App.styled';
+import { Controls } from './Controls/Controls';
+
+
+// export const App = () => {
+//   return (
+//     <Container>
+//     <h1>Please Leave Feedback</h1>
+//     <Controls></Controls>
+//
+//     </Container>
+//   );
+// };
+
+export class App extends Component {
+
+  constructor() {//для початку без спрощених варіантів
+    super();
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  };
+
+  addGoodVote = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  addNeutralVote = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  addBadVote = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    if (this.state.good === 0 && this.state.neutral === 0 && this.state.bad === 0) {
+      return 0;
+    }
+    return Math.round(this.state.good / (this.state.good + this.state.neutral + this.state.bad) * 100);
+  }
+
+  render() {
+    return (
+      <Container>
+        <h1>Please Leave Feedback</h1>
+        <Controls
+          clickGoodVote={this.addGoodVote}
+          clickNeutralVote={this.addNeutralVote}
+          clickBadVote={this.addBadVote}
+        ></Controls>
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+        <p>Total: {this.countTotalFeedback()}</p>
+        <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
+      </Container>
+    );
+  }
+
+}
+
+// export class App;
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  //flex-wrap: wrap;
+  flex-direction: column;
+  padding: 90px;
+  justify-content: left;
+  align-items: flex-start;
+  color: #010101
+`;
