c2f8d97d9d64a10d221e212710baf2a16158da58
YurGoV <yurgov@gmail.com>
2022-12-15 19:52:49 +0200

    style-correcting

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -4,8 +4,11 @@ export const SectionStyle = styled.section`
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
+  max-width: 400px;
   padding: 60px;
-  color: #010101
+  color: #010101;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 export const Title = styled.h1`
