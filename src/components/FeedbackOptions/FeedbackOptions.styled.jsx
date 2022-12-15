2a621f07cde169cfbdace489d0443a9eff6a3372
YurGoV <yurgov@gmail.com>
2022-12-15 20:36:44 +0200

    buttons-styled

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -4,6 +4,19 @@ export const ButtonsStyle = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
-  max-width: 200px;
+  max-width: 280px;
   margin: 10px 0;
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    background-color: white;
+    border: 1px solid lightgrey;
+    border-radius: 3px;
+    font-weight: bold;
+    height: 30px;
+    min-width: 80px;
+  }
 `;
